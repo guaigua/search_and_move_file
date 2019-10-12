83d0f145d4815f9a6d4781af319fc7a154e2ce94
German Guaigua <gerguaigua@gmail.com>
2019-10-12 13:36:30 -0500

    Add file for individual treatment

--- searchandmove_cron.py
+++ searchandmove_cron.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """
-Info
+Info Program Search and move for cron
 """
 #imports
+
 import sys
 import os
 import glob
@@ -10,8 +11,6 @@ import shutil
 import logging
 import textract
 import re
-
-
 
 # start editable vars 
 
@@ -23,6 +22,7 @@ files_success = 0                               # count files
 files_with_errors = 0							# count files with errors
 size = 0.0                                      # size
 found = ''										# found is empty
+pattern_to_search ='Fecha:(.+?) Hora:'          # pattern to search
 # end editable vars
 
 # start function definitions 
@@ -37,16 +37,16 @@ def log(level,msg,tofile=True):
 			logger.info(msg)
 		else:
 			logger.error(msg)
-#end log			
+	
 def end(code):
 	log(0,"End...")	
-	log(0,"-------------------------")
+	log(0,"----------------------------------------------------------------------------------")
 
-###
+
 def extractext(file):	
 	try:
 		text = str(textract.process(file))
-		found = re.search('Fecha:(.+?) Hora:', text).group(1)
+		found = re.search(pattern_to_search, text).group(1)
 		print (found)
 	except AttributeError:
 		found = ''
@@ -54,33 +54,47 @@ def extractext(file):
 # end function definitions 
 
 
-# start process with log #
+# start process #
 logger = logging.getLogger("cuarch")
 hdlr = logging.FileHandler(logfile)
 hdlr.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s: %(message)s'))
 logger.addHandler(hdlr) 
 logger.setLevel(logging.INFO)
-log(0,"Initialising...")
+log(0,"Initialising Cron...")
 
 for filename in glob.glob1(original_folder, "*.*"):
-	destfile = os.path.join(new_folder, filename)
-	srcfile = os.path.join(original_folder, filename)
+	folder=os.path.splitext(filename)[0]
+	srcfile = os.path.join(original_folder, filename)	
 	errorfile = os.path.join(error_folder, filename)
-	if os.stat(srcfile):
-		if not os.path.isfile(destfile):
-			extractext(srcfile)
-			print (extractext(found))
-			if found:				
-				size = size + (os.path.getsize(srcfile) / (1024*1024.0))
+	if os.stat(srcfile):		
+		found = extractext(srcfile)			
+		if found:	
+			size = size + (os.path.getsize(srcfile) / (1024*1024.0))
+			named = os.path.join(new_folder,folder)
+			try:
+				os.mkdir(named)
+				log(0,"Successfully created the directory %s " % named)
+
+			except OSError:
+				log(1,"Creation of the directory %s failed" % named)
+			
+			
+			destfile = os.path.join(named, filename)
+			destfile			
+			if not os.path.exists(destfile):
 				shutil.move(srcfile, destfile)
 				log(0,"Archived '" + filename + "'.")
 				files_success = files_success + 1
 			else:
 				shutil.move(srcfile, errorfile)
-				log(1,"Archived '" + filename + "'.")
+				log(1,"File Exists '" + filename + "'.")
 				files_with_errors = files_with_errors + 1
+		else:
+			shutil.move(srcfile, errorfile)
+			log(1,"Archived '" + filename + "'.")
+			files_with_errors = files_with_errors + 1
 
-log(0,"Successfully achieved" + str(files_success) + " files, totalling " + str(round(size,2)) + "MB. files with errors" + str(files_with_errors))
+log(0,"Successfully achieved " + str(files_success) + " files, totalling " + str(round(size,2)) + "MB. files with errors " + str(files_with_errors))
 end(0)
 
 
